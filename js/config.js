ced8fa35801792f949af9e5137c82460e9ab0fd2
blackshroud <blackshroud@gmail.com>
2016-01-03 19:35:35 +1030

    removing unneeded data

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
-var FORCAST_API_KEY = "76c365a2693a365026d8cde461b8f1b9";
+var FORCAST_API_KEY = "Add API key";
 var HUE_BASE = "http://{IP_ADDRESS_OF_BRIDGE}/api/{Username}/";
 var config = {
-    news: {feed: 'https://www.facebook.com/feeds/notifications.php?id=586844777&viewer=586844777&key=AWj-SmKL2UQQjs0p&format=rss20'
+    news: {feed: 'add RSS feed'
     },
     calendar: {
     maximumEntries: 10,
