5b305c45df3acd7c90639f999c023bb12ba6917e
blackshroud <blackshroud@gmail.com>
2016-01-03 13:59:27 +1030

    removing stuff

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-var FORCAST_API_KEY = "76c365a2693a365026d8cde461b8f1b9";
+var FORCAST_API_KEY = "insert key here";
 var HUE_BASE = "http://{IP_ADDRESS_OF_BRIDGE}/api/{Username}/";
-var config = {news: {feed: 'https://www.facebook.com/feeds/notifications.php?id=586844777&viewer=586844777&key=AWj-SmKL2UQQjs0p&format=rss20'}};
+var config = {news: {feed: 'rss feed location'}};
