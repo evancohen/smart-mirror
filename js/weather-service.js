fbb52d58fd160a2fcc6bbf0a3638e212dd63f8da
yaydogan <y.aydogan@gmail.com>
2016-02-11 08:55:14 -0500

    Adding weather icons
    Changes:
    * Added weather icons
    * Displaying temperature with single decimal digit with degree sign
    * Added daily temperature summary to the weather summary text

--- js/weather-service.js
+++ js/weather-service.js
@@ -20,6 +20,8 @@
                 return null;
             }
             service.forcast.data.currently.day = moment.unix(service.forcast.data.currently.time).format('ddd')
+            service.forcast.data.currently.temperature = parseFloat(service.forcast.data.currently.temperature).toFixed(1);
+            service.forcast.data.currently.wi = "wi-forecast-io-" + service.forcast.data.currently.icon;
             return service.forcast.data.currently;
         }
 
@@ -30,10 +32,21 @@
             // Add human readable info to info
             for (var i = 0; i < service.forcast.data.daily.data.length; i++) {
                 service.forcast.data.daily.data[i].day = moment.unix(service.forcast.data.daily.data[i].time).format('ddd');
+                service.forcast.data.daily.data[i].temperatureMin = parseFloat(service.forcast.data.daily.data[i].temperatureMin).toFixed(1);
+                service.forcast.data.daily.data[i].temperatureMax = parseFloat(service.forcast.data.daily.data[i].temperatureMax).toFixed(1);
+                service.forcast.data.daily.data[i].wi = "wi-forecast-io-" + service.forcast.data.daily.data[i].icon;
             };
             return service.forcast.data.daily;
         }
-
+		
+        service.hourlyForcast = function() {
+            if(service.forcast === null){
+                return null;
+            }
+            service.forcast.data.hourly.day = moment.unix(service.forcast.data.hourly.time).format('ddd')
+            return service.forcast.data.hourly;
+        }
+		
         service.refreshWeather = function(){
             return service.init(geoloc);
         }
