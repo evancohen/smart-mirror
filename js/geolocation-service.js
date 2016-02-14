11f26c30a3b070f474ad34e250a35789d3a9e57f
newtux <newtux@gmail.com>
2016-02-14 16:24:51 -0500

    Cache Geolocation Result
    Geolocation result is now cached the first time we call it.

--- js/geolocation-service.js
+++ js/geolocation-service.js
@@ -3,6 +3,7 @@
 
     function GeolocationService($q,$rootScope,$window) {
         var service = {};
+        var geoloc = null;
         var geolocation_msgs = {
         'errors.location.unsupportedBrowser':'Browser does not support location services',
         'errors.location.permissionDenied':'You have rejected access to your location',
@@ -13,36 +14,43 @@
         service.getLocation = function (opts) {
         var deferred = $q.defer();
         if ($window.navigator && $window.navigator.geolocation) {
-          $window.navigator.geolocation.getCurrentPosition(function(position){
-            $rootScope.$apply(function(){deferred.resolve(position);});
-          }, function(error) {
-            switch (error.code) {
-              case 1:
-                $rootScope.$broadcast('error',geolocation_msgs['errors.location.permissionDenied']);
-                $rootScope.$apply(function() {
-                  deferred.reject(geolocation_msgs['errors.location.permissionDenied']);
-                });
-                break;
-              case 2:
-                $rootScope.$broadcast('error',geolocation_msgs['errors.location.positionUnavailable']);
-                $rootScope.$apply(function() {
-                  deferred.reject(geolocation_msgs['errors.location.positionUnavailable']);
-                });
-                break;
-              case 3:
-                $rootScope.$broadcast('error',geolocation_msgs['errors.location.timeout']);
-                $rootScope.$apply(function() {
-                  deferred.reject(geolocation_msgs['errors.location.timeout']);
-                });
-                break;
-            }
-          }, opts);
+          if(geoloc !== null){
+            console.log("Cached Geolocation", geoloc);
+            return(geoloc);
+          }
+          else {
+            $window.navigator.geolocation.getCurrentPosition(function(position){
+              $rootScope.$apply(function(){deferred.resolve(position);});
+            }, function(error) {
+              switch (error.code) {
+                case 1:
+                  $rootScope.$broadcast('error',geolocation_msgs['errors.location.permissionDenied']);
+                  $rootScope.$apply(function() {
+                    deferred.reject(geolocation_msgs['errors.location.permissionDenied']);
+                  });
+                  break;
+                case 2:
+                  $rootScope.$broadcast('error',geolocation_msgs['errors.location.positionUnavailable']);
+                  $rootScope.$apply(function() {
+                    deferred.reject(geolocation_msgs['errors.location.positionUnavailable']);
+                  });
+                  break;
+                case 3:
+                  $rootScope.$broadcast('error',geolocation_msgs['errors.location.timeout']);
+                  $rootScope.$apply(function() {
+                    deferred.reject(geolocation_msgs['errors.location.timeout']);
+                  });
+                  break;
+              }
+            }, opts);
+          }
         }
         else
         {
           $rootScope.$broadcast('error',geolocation_msgs['errors.location.unsupportedBrowser']);
           $rootScope.$apply(function(){deferred.reject(geolocation_msgs['errors.location.unsupportedBrowser']);});
         }
+        geoloc = deferred.promise;
         return deferred.promise;
       }
         
