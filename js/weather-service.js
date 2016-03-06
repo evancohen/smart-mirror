d8a1fa6d7d0ed12bba90b2480197d4349063b668
kxd1000 <kurt.trattner@me.com>
2016-03-06 21:10:11 +0100

    weather forcast language added

--- js/weather-service.js
+++ js/weather-service.js
@@ -9,8 +9,8 @@
         service.init = function(geoposition) {
             geoloc = geoposition;
             return $http.jsonp('https://api.forecast.io/forecast/'+config.forcast.key+'/'+
-                    geoposition.coords.latitude+','+geoposition.coords.longitude+'?units=' + 
-                    config.forcast.units + "&callback=JSON_CALLBACK")
+                    geoposition.coords.latitude+','+geoposition.coords.longitude+'?units='+
+                    config.forcast.units+'&lang='+config.forcast.lang+"&callback=JSON_CALLBACK")
                 .then(function(response) {
                     return service.forcast = response;
                 });
