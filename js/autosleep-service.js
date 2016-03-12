db4ec501494c972a43e690eb03c521d25470bfe4
Missionrulz <missionrulz@gmail.com>
2016-03-12 14:57:21 +1300

    remove $scope from service

--- js/autosleep-service.js
+++ js/autosleep-service.js
@@ -1,13 +1,12 @@
 (function() {
     'use strict';
 
-    function AutoSleepService($interval, $filter) {
+    function AutoSleepService($interval) {
         var service = {};
         var autoSleepTimer;
 
-        service.startAutoSleepTimer = function($scope) {
+        service.startAutoSleepTimer = function() {
             if (typeof config.autoTimer !== 'undefined' && typeof config.autoTimer.autosleep !== 'undefined' && typeof config.autoTimer.autowake !== 'undefined') {
-                service.scope = $scope;
                 service.stopAutoSleepTimer();
                 autoSleepTimer = $interval(service.sleep, config.autoTimer.autosleep);
                 console.debug('Starting auto-sleep timer', config.autoTimer.autosleep);
@@ -19,22 +18,12 @@
             $interval.cancel(autoSleepTimer);
         };
 
-        service.checkWakeUp = function() {
-            if (typeof config.autoTimer !== 'undefined' && typeof config.autoTimer.autowake !== 'undefined' && config.autoTimer.autowake == $filter('date')(service.scope.date, 'HH:mm:ss')) {
-                console.debug('Auto-wake', config.autoTimer.autowake);
-                service.wake();
-                service.startAutoSleepTimer(service.scope);
-            }
-        };
-
         service.wake = function() {
             exec("/opt/vc/bin/tvservice -p", puts);
-            service.scope.focus = "default";
         };
 
         service.sleep = function() {
             exec("/opt/vc/bin/tvservice -o", puts);
-            service.scope.focus = "sleep";
         };
 
         return service;
