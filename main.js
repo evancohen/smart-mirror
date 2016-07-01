1f7b895043c11914f5f5a717b72a4e5deeba911a
justbill2020 <bmartin@salientcdi.com>
2016-07-01 15:23:37 -0500

    main.js syntax err

--- main.js
+++ main.js
@@ -133,7 +133,7 @@ kwsProcess.stdout.on('data', function (data) {
         mainWindow.webContents.send('motion_stdout', data.toString)
     })
   }
-}
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
