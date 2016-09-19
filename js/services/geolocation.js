7dfdfd9214f08bc43f6681f634dd4abbe5b9ad48
Grant Magdanz <grantmagdanz@gmail.com>
2016-09-18 18:23:11 -0700

    Switch to Google's API

--- js/services/geolocation.js
+++ js/services/geolocation.js
@@ -12,12 +12,6 @@
     function GeolocationService($q,$rootScope,$window,$http) {
         var service = {};
         var geoloc = null;
-        var geolocation_msgs = {
-        'errors.location.unsupportedBrowser':'Browser does not support location services',
-        'errors.location.permissionDenied':'You have rejected access to your location',
-        'errors.location.positionUnavailable':'Unable to determine your location',
-        'errors.location.timeout':'Service timeout has been reached'
-        }
 
         service.getLocation = function (opts) {
         var deferred = $q.defer();
@@ -40,11 +34,10 @@
               return(geoloc);
             }
 
-            $http.get("http://ipinfo.io").then(
-              function(ipinfo){
-                var loc = angular.fromJson(ipinfo).data.loc
-                var latLong = loc.split(",")
-                deferred.resolve({'coords': {'latitude': latLong[0], 'longitude':latLong[1]}})
+            $http.get("https://maps.googleapis.com/maps/api/browserlocation/json?browser=chromium").then(
+              function(result){
+                var location = angular.fromJson(result).data.location
+                deferred.resolve({'coords': {'latitude': location.lat, 'longitude':location.lng}})
               },
               function(err) {
                 console.debug("Failed to retrieve geolocation.")
