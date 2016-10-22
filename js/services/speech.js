ab90ebef4f15e4bd518f58dcb4c77618e2c4ea98
Evan Cohen <evanbtcohen@gmail.com>
2016-10-21 22:41:12 -0700

    update annyang. closes #392

--- js/services/speech.js
+++ js/services/speech.js
@@ -1,39 +1,41 @@
 const {ipcRenderer} = require('electron');
 
-(function(annyang) {
+(function () {
     'use strict';
 
     function SpeechService($rootScope) {
-        var service = {};
-        var callbacks = {};
-   
-        service.init = function(cb) {
-            // Set annyang language defined in the config file
-            annyang.setLanguage((typeof config.language != 'undefined')?config.language : 'en-US');
-            annyang.debug(false);
-            
+        var service = {}
+        var callbacks = {}
+
+        service.init = function (cb) {
+            // workaround so we can trigger requests at any time 
+            annyang.isListening = () => {return true}
+            // Set lenguage and debug state
+            annyang.setLanguage((typeof config.language != 'undefined') ? config.language : 'en-US')
+            annyang.debug(false)
+
             // add specified callback functions
             if (isCallback(cb.listening)) {
-                callbacks.listening = function(bool){
-                    $rootScope.$apply(cb.listening(bool));
+                callbacks.listening = function (bool) {
+                    $rootScope.$apply(cb.listening(bool))
                 }
             }
             if (isCallback(cb.partialResult)) {
-                callbacks.partialResult = function(data){
-                    $rootScope.$apply(cb.partialResult(data));
+                callbacks.partialResult = function (data) {
+                    $rootScope.$apply(cb.partialResult(data))
                 }
             }
             if (isCallback(cb.finalResult)) {
-                callbacks.finalResult = function(data){
-                    $rootScope.$apply(cb.finalResult(data));
+                callbacks.finalResult = function (data) {
+                    $rootScope.$apply(cb.finalResult(data))
                 }
             }
             if (isCallback(cb.error)) {
-                callbacks.error = function(data){
-                    $rootScope.$apply(cb.error(data));
+                callbacks.error = function (data) {
+                    $rootScope.$apply(cb.error(data))
                 }
             }
-            
+
             ipcRenderer.on('hotword', (event, spotted) => {
                 callbacks.listening(true)
             })
@@ -44,39 +46,39 @@ const {ipcRenderer} = require('electron');
 
             ipcRenderer.on('final-results', (event, text) => {
                 callbacks.finalResult(text)
-                annyang.simulate(text)
+                annyang.trigger(text)
                 callbacks.listening(false)
             })
-            
+
         }
-        
+
         // Ensure callback is a valid function
-        function isCallback(callback){
-            return typeof(callback) == "function";
+        function isCallback(callback) {
+            return typeof (callback) == "function";
         }
-        
+
         // COMMANDS
-        service.commands = {};
-        service.addCommand = function(phrase, callback) {
-            var command = {};
+        service.commands = {}
+        service.addCommand = function (phrase, callback) {
+            var command = {}
 
             // Wrap annyang command in scope apply
-            command[phrase] = function(arg1, arg2) {
-                $rootScope.$apply(callback(arg1, arg2));
+            command[phrase] = function (arg1, arg2) {
+                $rootScope.$apply(callback(arg1, arg2))
             };
 
             // Extend our commands list
-            angular.extend(service.commands, command);
+            angular.extend(service.commands, command)
 
             // Add the commands to annyang
-            annyang.addCommands(service.commands);
-            console.debug('added command "' + phrase + '"', service.commands);
+            annyang.addCommands(service.commands)
+            console.debug('added command "' + phrase + '"', service.commands)
         };
 
         return service;
     }
 
     angular.module('SmartMirror')
-        .factory('SpeechService', SpeechService);
+        .factory('SpeechService', SpeechService)
 
-}(window.annyang));
+} (window.annyang));
