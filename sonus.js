3dc16070e383e1c9b0db4299cc6cd06d267df695
Bill Martin <bmartin@salientcdi.com>
2016-12-21 02:24:58 -0600

    remove projectId requirement in config.
    sonus.js will now use the keyfile to load the project ID.
    
    sonus.js will now check to make sure the model file exists and default to snowboy.umdl if it does not.

--- sonus.js
+++ sonus.js
@@ -1,29 +1,41 @@
 'use strict'
-
 // Load in smart mirror config
-const config = require(__dirname + "/config.js")
-if(!config || !config.speech || !config.speech.keyFilename || !config.speech.model || !config.language){
-  throw "Configuration Error! See: https://docs.smart-mirror.io/docs/configure_the_mirror.html#speech";
+const fs = require('fs')
+var config = require(__dirname + "/config-index.js")
+
+if(!config || !config.speech || !config.speech.keyFilename || !config.speech.hotwords[0].model || !config.general.language){
+  throw "Configuration Error! See: https://docs.smart-mirror.io/docs/configure_the_mirror.html#speech"
 }
 
+var keyFile = JSON.parse(fs.readFileSync(config.speech.keyFilename,"utf8"))
+var umdl = __dirname + '/node_modules/sonus/resources/snowboy.umdl'
 // Configure Sonus
 const Sonus = require('sonus')
 const speech = require('@google-cloud/speech')({
-  projectId: config.speech.projectId,
+  projectId: keyFile.project_id,
   keyFilename: config.speech.keyFilename
 })
 
 let hotwords = []
 
+
 if(typeof config.speech.model == 'string'){
-  hotwords.push({ file: config.speech.model, hotword: config.speech.keyword, sensitivity:  config.speech.sensitivity || '0.5'})
+  if (fs.existsSync(config.speech.model)) {    
+    hotwords.push({ file: config.speech.model, hotword: config.speech.keyword, sensitivity:  config.speech.sensitivity || '0.5'})
+  } else {
+    hotwords.push({ file: umdl, hotword: 'snowboy', sensitivity:  config.speech.sensitivity || '0.5'})
+  }
 } else {
   for(let i =0; i < config.speech.model.length; i++){
-    hotwords.push({ file: config.speech.model[i], hotword: config.speech.keyword[i], sensitivity:  config.speech.sensitivity || '0.5'})
+    if (fs.existsSync(config.speech.model[i])) {
+      hotwords.push({ file: config.speech.model[i], hotword: config.speech.keyword[i], sensitivity:  config.speech.sensitivity || '0.5'})
+    } else {
+      hotwords.push({ file: umdl, hotword: 'snowboy', sensitivity:  config.speech.sensitivity || '0.5'})
+    }
   }
 }
 
-const language = config.language
+const language = config.general.language
 const sonus = Sonus.init({ hotwords, language }, speech)
 
 // Start Recognition
