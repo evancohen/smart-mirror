6e062f1c0fc8b4e3497ac1190a3e06e6079d6ad3
Bill Martin <bmartin@salientcdi.com>
2017-01-12 00:05:03 -0600

    Jb/hotfix 510, 524 and sleeping (#529)
    * test output
    
    * fixes #510, #524, and smart-mirror not sleeping
    
    this fixes the issue with #510, #524 and a issue from user on discord regarding smart-mirror not sleeping after specificed amount of minutes.
    
    * changed $scope.$parent.focus to Focus.change
    
    * Update config.schema.json

--- plugins/autosleep/controller.js
+++ plugins/autosleep/controller.js
@@ -1,26 +1,12 @@
-function AutoSleep($scope, $http, $q, SpeechService,AutoSleepService) {
+function AutoSleep($http, $q, SpeechService,AutoSleepService) {
 
 
     // Hide everything and "sleep"
     SpeechService.addCommand('sleep', function () {
         console.debug("Ok, going to sleep...");
         AutoSleepService.sleep();
-        $scope.$parent.focus = AutoSleepService.scope;
     });
 
-    $scope.$on('autoSleep.wake', function(e, value) {
-        if (!AutoSleepService.woke) {
-		    AutoSleepService.wake();
-            $scope.$parent.focus = AutoSleepService.scope; 
-	    }
-	    console.debug('controller Wake');
-	    AutoSleepService.stopAutoSleepTimer();
-    }); 
-    $scope.$on('autoSleep.sleep', function(e, value) {
-        AutoSleepService.sleep();
-        $scope.$parent.focus = AutoSleepService.scope; 
-	    console.debug('controller sleep');
-    });
 }
 
 angular.module('SmartMirror')
