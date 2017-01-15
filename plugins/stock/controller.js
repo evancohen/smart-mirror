b4ec6e9e9ec53ad03ba6123b7028de429eb0ce9e
Danny Luu <hisoka365@gmail.com>
2017-01-14 21:08:22 -0500

    Remove error when Stock names are not filled.

--- plugins/stock/controller.js
+++ plugins/stock/controller.js
@@ -1,8 +1,12 @@
 function Stock($scope, $http, $q, $interval) {
 
+    // I don't see the default refresh interval being set in the JSON object...
+    var defaultRefreshInterval = 100;
+
     var getStockQuotes = function () {
         var deferred = $q.defer();
-        if (config.stock.names.length) {
+
+        if (!!config.stock && config.stock.names.length) {
             var url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quote%20where%20symbol%20in%20(' + "'" + config.stock.names.join("','") + "'" + ')&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&format=json';
 
             $http.get(url).then(function (response) {
@@ -30,7 +34,8 @@ function Stock($scope, $http, $q, $interval) {
     }
 
     getStocks();
-    $interval(getStocks, config.stock.refreshInterval * 60000 || 1800000)
+    $interval(getStocks, defaultRefreshInterval * 60000 || 1800000)
+    //$interval(getStocks, config.stock.defaultRefreshInterval * 60000 || 1800000)
 }
 
 angular.module('SmartMirror')
