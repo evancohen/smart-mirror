4019aa782b0f7b5b6e3476ca1d711ae2c0cffdb6
Danny Luu <hisoka365@gmail.com>
2017-01-16 23:29:46 -0500

    Update bashrc with subshell function.

--- scripts/pi-install.sh
+++ scripts/pi-install.sh
@@ -150,12 +150,9 @@ if ! grep -q '(smart-mirror)' ~/.config/lxsession/LXDE-pi/autostart; then
 fi
 
 # Add start commands in the user's bashrc.
-echo "export MIRROR_HOME=\"~/smart-mirror\"" >> ~/.bashrc
-echo "alias update-mirror='cd \$MIRROR_HOME && git pull && npm install'" >> ~/.bashrc
-echo "alias start-mirror='cd \$MIRROR_HOME && npm start'" >> ~/.bashrc
-echo "alias dev-mirror='cd \$MIRROR_HOME && npm start dev'" >> ~/.bashrc
-echo "alias ssh-start-mirror='cd \$MIRROR_HOME && DISPLAY=:0 npm start'" >> ~/.bashrc
-echo "alias ssh-dev-mirror='cd \$MIRROR_HOME && DISPLAY=:0 npm start dev'" >> ~/.bashrc
+echo "export MIRROR_HOME=~/smart-mirror" >> ~/.bashrc
+echo "run_mirror () { ( cd \$MIRROR_HOME && DISPLAY=:0 npm \"\$@\" ); }" >> ~/.bashrc
+echo "alias mirror=run_mirror" >> ~/.bashrc
 
 # The mirror is now installed, yay!
 cat << "EOF"
