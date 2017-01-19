e1ff0f957229b3f0d57f0f37c4e7615e328ee428
Evan Cohen <evanbtcohen@gmail.com>
2017-01-18 19:26:14 -0800

    moving to tabs

--- plugins/autosleep/controller.js
+++ plugins/autosleep/controller.js
@@ -2,10 +2,10 @@ function AutoSleep($http, $q, SpeechService,AutoSleepService) {
 
 
     // Hide everything and "sleep"
-    SpeechService.addCommand('sleep', function () {
-        console.debug("Ok, going to sleep...");
-        AutoSleepService.sleep();
-    });
+	SpeechService.addCommand('sleep', function () {
+		console.debug("Ok, going to sleep...");
+		AutoSleepService.sleep();
+	});
 
 }
 
