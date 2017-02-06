b948e353db2f006bbb058ef6441463d7ddd13913
Bill Martin <bmartin@salientcdi.com>
2017-02-06 12:42:24 -0600

    Move DannyLuu:dev (#577) to evancohen:dl/fitbitSquash
    * removed "npm" and "install" packages from dependencies
    
    * fix for conf-audio.sh
    
    removes the comment line from exported file contents
    
    * Update config.schema.json
    
    fixes #519 @dannyluu i just updated the part that allows it to save... the UI still needs to be fixed... please keep working on it...
    
    * Add missing brace. (#542)
    
    * Adding eslint + travis (#548)
    
    * Adding eslint + travis
    
    * Remove Dependency CI and show build status
    
    * Fixing path problems and first run (#549)
    
    * use path.resolve() to get files
    
    fixing up sonus.js
    
    * updating speech config schema
    
    * remove umdl beacuse we aren't using it anymore
    
    * fix sonus error on first run
    
    * better error message on first run
    
    * oh no. :cold_sweat:
    
    * don't use kiosk mode when started with dev :rocket:
    
    * Remove error when Stock names are not filled. (#551)
    
    * Remove error when Stock names are not filled.
    
    * Add missing semi-colon
    
    * Set refresh to 30mins.
    
    * Fix browser console errors with null tv show lists. (#552)
    
    * Fix browser console errors with null tv show lists.
    
    * Fix build error. I am new to angular?!
    
    * Angular doesn't need double negations?
    
    * Fixed error with language parameter and weather (#550)
    
    * Fixed typo in language variable name
    
    * Fixed typo in language variable name
    
    * Fixed wrong moment.js lang parameter
    
    * Changed handling of the language variable handling for moment.js
    
    * v0.0.10
    
    * Create alias for starting the smart mirror from anywhere. (#555)
    
    * Create alias for starting the smart mirror from anywhere.
    
    * Update smart mirror!
    
    * Update bashrc with subshell function.
    
    * Reload bash profile upon install.
    
    * Update install message to reflect new changes
    
    * fixing fitbit
    
    * Add english resource for Fitbit.
    
    * Cycle through Fitbit statistics.
    
    * Update controller documentation and optimized service calls.
    
    * Update fitibt css for progress bar.
    
    * Add floors to Today's stats and fixed other tags.
    
    * Update fitbit EN resource.
    
    * Shortened the progress bar.
    
    * Update init logic... my bad.
    
    * Show progress bar on daily stats.
    
    * Update progress bar ot turn green when 100%
    
    * Localizing units
    
    * Update ui to display green progress bar when over 100%
    
    * Adding comments for new enpoints that I would like to integrate.
    
    * Update english locale.
    
    * Update service and controller with new api requests.
    
    * Update fitbit index html to incorporate controller function calls.
    
    * Adding all changes.
    
    * Setting groups to cycle.
    
    * Finalizating changes.
    
    * Updated views on data displayed.
    
    * Add message resources
    
    Resources added for the languanges. WIll need translations
    
    * Replaced spaces with tabs
    
    * Found the culprit!

--- scripts/pi-install.sh
+++ scripts/pi-install.sh
@@ -151,7 +151,7 @@ fi
 
 # Add start commands in the user's bashrc.
 echo "export MIRROR_HOME=~/smart-mirror" >> ~/.bashrc
-echo "run_mirror () { ( cd \$MIRROR_HOME && DISPLAY=:0 npm run \"\$@\" ); }" >> ~/.bashrc
+echo "run_mirror () { ( cd \$MIRROR_HOME && DISPLAY=:0 npm \"\$@\" ); }" >> ~/.bashrc
 echo "alias mirror=run_mirror" >> ~/.bashrc
 cd ~ && source .bashrc
 
