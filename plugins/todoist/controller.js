9f62c517be82c0ff982c9986b62dd8250f3b31d8
overflOw11 <jeremy.vigneron@gmail.com>
2017-02-08 19:27:00 +0100

    Todoist integration (view specific project and add item to it) + french corrections

---
+++ plugins/todoist/controller.js
@@ -0,0 +1,37 @@
+function Todoist($scope, TodoistService, SpeechService, Focus) {
+
+	// Show project
+	SpeechService.addCommand('todoist_show', function (project) {
+		console.debug("Show todoist project");
+		getTodoist(project)
+		Focus.change("todoist");
+	});
+
+	// Add element to project
+	SpeechService.addCommand('todoist_add', function (element, project) {
+		console.debug("Add element to todoist project");
+		addTodoist(element, project);
+		Focus.change("todoist");
+	});
+
+	var getTodoist = function(project){
+		TodoistService.renderTasks(project).then(function (items) {
+			$scope.todoist = items;
+		}, function (error) {
+			$scope.todoist = { error: error };
+		});
+	}
+
+	var addTodoist = function(element, project){
+		// TodoistService.addTask(element, project);
+		// getTodoist(project);
+		TodoistService.addTask(element, project).then(function () {
+			getTodoist(project)
+		}, function (error) {
+			$scope.todoist = { error: error };
+		});
+	}
+}
+
+angular.module('SmartMirror')
+    .controller('Todoist', Todoist);
