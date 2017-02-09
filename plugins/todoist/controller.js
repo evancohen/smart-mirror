d0f20a054c8c57f798a2d2329a846f9cb679133d
overflOw11 <jeremy.vigneron@gmail.com>
2017-02-09 21:52:45 +0100

    Possibility to remove item from todoist project

--- plugins/todoist/controller.js
+++ plugins/todoist/controller.js
@@ -10,22 +10,35 @@ function Todoist($scope, TodoistService, SpeechService, Focus) {
 	// Add element to project
 	SpeechService.addCommand('todoist_add', function (element, project) {
 		console.debug("Add element to todoist project");
-		addTodoist(element, project);
+		addItemTodoist(element, project);
+		Focus.change("todoist");
+	});
+
+	// Remove element from project
+	SpeechService.addCommand('todoist_remove', function (element, project) {
+		console.debug("Remove element from todoist project");
+		removeItemTodoist(element, project);
 		Focus.change("todoist");
 	});
 
 	var getTodoist = function(project){
-		TodoistService.renderTasks(project).then(function (items) {
+		TodoistService.getItemsFromProject(project).then(function (items) {
 			$scope.todoist = items;
 		}, function (error) {
 			$scope.todoist = { error: error };
 		});
 	}
 
-	var addTodoist = function(element, project){
-		// TodoistService.addTask(element, project);
-		// getTodoist(project);
+	var addItemTodoist = function(element, project){
 		TodoistService.addTask(element, project).then(function () {
+			getTodoist(project)
+		}, function (error) {
+			$scope.todoist = { error: error };
+		});
+	}
+
+	var removeItemTodoist = function(element, project){
+		TodoistService.completeTask(element, project).then(function () {
 			getTodoist(project)
 		}, function (error) {
 			$scope.todoist = { error: error };
