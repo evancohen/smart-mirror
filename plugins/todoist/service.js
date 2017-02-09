5a5039806fdad976facc1feb174d15b41b9840e7
overflOw11 <jeremy.vigneron@gmail.com>
2017-02-09 23:57:14 +0100

    Fix completeTask() function and travis errors

--- plugins/todoist/service.js
+++ plugins/todoist/service.js
@@ -25,7 +25,7 @@
 					data: $httpParamSerializer({token: config.todoist.key, commands: '[{"type": "item_add", "temp_id": "", "uuid":"' + service.generateUUID() + '", "args": {"content": "' + item + '", "project_id": "' + project_id + '"}}]'})
 				};
 				$http(req).then(function() {
-			    	deferred.resolve();
+					deferred.resolve();
 				});
 			});
 			return deferred.promise;
@@ -34,7 +34,8 @@
 		service.completeTask = function(item, project) {
 			var deferred = $q.defer();
 			service.requestProjectsWithItems().then(function (response) {
-				var item_ids = service.getItemIds(item, response);
+				var project_id = service.getProjectId(project, response);
+				var item_ids = service.getItemIds(item, project_id, response);
 				var req = {
 					method: 'POST',
 					url: 'https://todoist.com/API/v7/sync',
@@ -44,7 +45,7 @@
 					data: $httpParamSerializer({token: config.todoist.key, commands: '[{"type": "item_complete", "uuid":"' + service.generateUUID() + '", "args": {"ids": [' + item_ids + ']}}]'})
 				};
 				$http(req).then(function() {
-			    	deferred.resolve();
+					deferred.resolve();
 				});
 			});
 			return deferred.promise;
@@ -52,7 +53,6 @@
 
 		service.requestProjectsWithItems = function(){
 			var deferred = $q.defer();
-			var response = [];
 			var req = {
 				method: 'POST',
 				url: 'https://todoist.com/API/v7/sync',
@@ -67,6 +67,8 @@
 			return deferred.promise;
 		}
 
+		// Get all items of thhe requested project.
+		// return a array of items object
 		service.getItemsOfProject = function(response, chosen_project){
 			var result = [];
 			var project_id = service.getProjectId(chosen_project, response);
@@ -78,6 +80,7 @@
 			return result;
 		}
 
+		// Get project id of the requested project.
 		service.getProjectId = function(project, response){
 			var project_id = -1;
 			console.debug("Looking for " + project + " in the list " + response.projects);
@@ -91,18 +94,22 @@
 			return project_id;
 		}
 
-		service.getItemIds = function(item, response){
+		// Get a a list of item ids which matching the requested id for the requested project.
+		// return a stringify list of item ids (ex : "134234","122343")
+		service.getItemIds = function(item, project_id, response){
 			var item_ids = "";
 			console.debug("Looking for " + item + " in the list " + response.items);
 			for (var i=0; i < response.items.length; i++){
-				if (response.items[i].content.toLowerCase() === item.toLowerCase()){
+				if (response.items[i].content.toLowerCase() === item.toLowerCase()
+					&& response.items[i].project_id === project_id){
 					item_ids = item_ids + '"' + response.items[i].id + '",';
 				}
 			}
 			console.debug("Found " + item + " in list -> " + item_ids);
-			return item_ids.substring(0, item_ids.length - 1);;
+			return item_ids.substring(0, item_ids.length - 1);
 		}
 
+		// Generate UUID for the Todoist API
 		service.generateUUID = function(){
 			var d = new Date().getTime();
 			if(window.performance && typeof window.performance.now === "function"){
