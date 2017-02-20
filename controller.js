d7f84b561c22c9744b94f93e37353a3db0ac9b44
sam detweiler <sdetweil@gmail.com>
2017-02-20 12:11:32 -0600

    one more time tabs and spaces

--- controller.js
+++ controller.js
@@ -1,25 +1,25 @@
 function Calendar($scope, $http, $interval, CalendarService) {
 
-    var getCalendar = function() {
-       if(typeof config.calendar.icals != 'undefined') {
-		// clear the events list
-		CalendarService.events = [];
-		// get the events from the calendars
-          CalendarService.getCalendarEvents(config.calendar.icals,CalendarService.events).then(function() {
-        // get the ones in scope (date range, and number of entries)	
-          $scope.calendar = CalendarService.getFutureEvents(CalendarService.events,config.calendar.maxDays,config.calendar.maxResults);
-          }, function(error) {
-             console.log(error);
-          });
+	var getCalendar = function() {
+		if(typeof config.calendar.icals != 'undefined') {
+			// clear the events list
+			CalendarService.events = [];
+			// get the events from the calendars
+			CalendarService.getCalendarEvents(config.calendar.icals,CalendarService.events).then(function() {
+				// get the ones in scope (date range, and number of entries)	
+				$scope.calendar = CalendarService.getFutureEvents(CalendarService.events,config.calendar.maxDays,config.calendar.maxResults);
+			}, function(error) {
+				console.log(error);
+			});
 		}
 		else
 		{
-             console.log("No iCals defined");
+			console.log("No iCals defined");
 		}
-    };
+	};
 
-    getCalendar();
-    $interval(getCalendar, config.calendar.refreshInterval * 60000 || 1800000)
+	getCalendar();
+	$interval(getCalendar, config.calendar.refreshInterval * 60000 || 1800000)
 }
 
 angular.module('SmartMirror')
