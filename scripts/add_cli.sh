a5bed17f1bfa7d1551255bd47412c8f7bfe0f133
Evan Cohen <evanbtcohen@gmail.com>
2017-03-03 20:15:07 -0800

    Only write to bashrc if the run_mirror function doesn't exist

--- scripts/add_cli.sh
+++ scripts/add_cli.sh
@@ -1,5 +1,10 @@
-# Add start commands in the user's bashrc.
-echo "export MIRROR_HOME=~/smart-mirror" >> ~/.bashrc
-echo "run_mirror () { ( cd \$MIRROR_HOME && DISPLAY=:0 npm run \"\$@\" ); }" >> ~/.bashrc
-echo "alias mirror=run_mirror" >> ~/.bashrc
+if ! grep -q 'run_mirror' ~/.bashrc; then
+    sed -i -e '$a\
+\
+#smart-mirror CLI\
+export MIRROR_HOME=~/smart-mirror\
+run_mirror () { ( cd $MIRROR_HOME && DISPLAY=:0 npm run "$@" ); }\
+alias mirror=run_mirror\' ~/.bashrc;
+fi
+
 cd ~ && source .bashrc
