a406ed912f189e3ef1cbcca57c8fb3456eb80f92
Sam Detweiler <sdetweil2gmail.com>
2017-03-08 10:50:09 -0600

    change calendar service to be re-entrant and allow different event filters, days and count

--- plugins/calendar/controller.js
+++ plugins/calendar/controller.js
@@ -1,12 +1,22 @@
 function Calendar($scope, $http, $interval, CalendarService) {
 
-	var getCalendar = function(){
-		CalendarService.getCalendarEvents().then(function () {
-			$scope.calendar = CalendarService.getFutureEvents();
-		}, function (error) {
-			console.log(error);
-		});
-	}
+	var getCalendar = function() {
+		if(typeof config.calendar.icals != 'undefined') {
+			// clear the events list
+			events = [];
+			// get the events from the calendars
+			CalendarService.getCalendarEvents(config.calendar.icals,events).then(function() {
+				// get the ones in scope (date range, and number of entries)	
+				$scope.calendar = CalendarService.getFutureEvents(events,config.calendar.maxDays,config.calendar.maxResults);
+			}, function(error) {
+				console.log(error);
+			});
+		}
+		else
+		{
+			console.log("No iCals defined");
+		}
+	};
 
 	getCalendar();
 	$interval(getCalendar, config.calendar.refreshInterval * 60000 || 1800000)
