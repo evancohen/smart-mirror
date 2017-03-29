77341b52ee565bc398f2ab26ce7d21d008aeced0
Bill Martin <bmartin@salientcdi.com>
2017-03-29 10:09:13 -0500

    add record device to dev (#624)
    * Adding eslint + travis (#548)
    
    * Adding eslint + travis
    
    * Remove Dependency CI and show build status
    
    * Fixing path problems and first run (#549)
    
    * use path.resolve() to get files
    
    fixing up sonus.js
    
    * updating speech config schema
    
    * remove umdl beacuse we aren't using it anymore
    
    * fix sonus error on first run
    
    * better error message on first run
    
    * oh no. :cold_sweat:
    
    * don't use kiosk mode when started with dev :rocket:
    
    * v0.0.10
    
    * correct error message for missing config
    
    * Mirror CLI fixes (#559)
    
    * check for dev flag at any position
    
    * alias mirror to npm run *
    
    * updating text at the end of the install script
    
    * config prompt update
    
    * moving to tabs
    
    * use config to specify recording device
    
    * update config schema
    
    
    init config.schema.json update
    * updated `speech/config.schema.json`
    * moved `config.default.json` and `config.schema.js` files to `remote` folder
    * renamed `config.default.json` to `.config.default.json` so that it is hidden on most default builds of linux... this is an attempt of keeping long time users from changing the file as was the old process when updating smart-mirror prior to remote configUI...
    * updated main.js, remote.js and index.html to reflect these changes
    
    use config to specify recording device
    
    update config schema
    
    
    init config.schema.json update
    * updated `speech/config.schema.json`
    * moved `config.default.json` and `config.schema.js` files to `remote` folder
    * renamed `config.default.json` to `.config.default.json` so that it is hidden on most default builds of linux... this is an attempt of keeping long time users from changing the file as was the old process when updating smart-mirror prior to remote configUI...
    * updated main.js, remote.js and index.html to reflect these changes
    
    use config to specify recording device
    
    * minor changes
    
    * add device functionality and range value updates
    
    * fix eslint issues
    
    * fix main after rebase
    
    * squashing mic input work
    
    * fix default configs

---
+++ remote/config.schema.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+const {resolve} = require('path')
+const {exec} = require('child_process')
+const pluginDir = resolve('./plugins');
+
+
+//TODO get general schema, then plugin schema
+
+function getConfigSchema(cb) {
+	let configSchema = { schema: {}, form: [], value: {} };
+	exec("arecord -l | grep -w 'card'", function (err, stdout) {
+		if (!err) {
+			fs.readdir(pluginDir, function (err, files) {
+				let l = files.length - 1;
+				for (var index = 0; index < files.length; ++index) {
+					var file = files[index];
+					if (file[0] !== '.') {
+						var filePath = pluginDir + '/' + file + '/config.schema.json';
+						fs.readFile(filePath, 'utf8', function (err, data) {
+							--l;
+							if (!err) {
+								let pluginConfigSchema = JSON.parse(data);
+								if (pluginConfigSchema.schema.speech) {
+									getAudioDevices(pluginConfigSchema, stdout)
+								}
+								Object.assign(configSchema.schema, pluginConfigSchema.schema)
+								if (pluginConfigSchema.form) { configSchema.form = configSchema.form.concat(pluginConfigSchema.form) }
+								if (pluginConfigSchema.value) { Object.assign(configSchema.value, pluginConfigSchema.value) }
+							}
+							!l && cb(configSchema)
+						});
+					}
+				}
+			});
+		}
+	});
+}
+function getAudioDevices(obj, stdO) {
+	var devOut = []
+	stdO.split("\n").forEach(function (option) {
+		if (option != "") {
+			let hwID = 'hw:' + option.match(/\d+(?=\:)/g).join(',')
+			let desc = option.match(/[^(?!\d\: )][a-zA-Z\[\]\d \,]+(?=\, device)/g).join(' ').replace(": ", "")
+			devOut.push({ hwID, desc })
+		}
+	})
+	devOut.forEach(function (dataItm) {
+		obj.schema.speech.properties.device.enum = ["default"]
+		obj.schema.speech.properties.device.enum.push(dataItm.hwID)
+		obj.form[0].items.forEach(function (formItm, formIdx) {
+			if (formItm.key == 'speech.device') {
+				formItm.titleMap = { default: "Default Device" }
+				formItm.titleMap[dataItm.hwID] = dataItm.desc
+				obj.form[0].items[formIdx] = formItm
+			} 
+		})
+	})
+}
+
+
+module.exports = getConfigSchema
