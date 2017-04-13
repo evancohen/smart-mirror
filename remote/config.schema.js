25eba047c9d3bb39d8afec7cd8e1a6f47f7c357b
Evan Cohen <evanbtcohen@gmail.com>
2017-04-13 09:11:33 -0700

    only detect candidate audio devices on linux devices

--- remote/config.schema.js
+++ remote/config.schema.js
@@ -9,30 +9,28 @@ const pluginDir = resolve('./plugins');
 function getConfigSchema(cb) {
 	let configSchema = { schema: {}, form: [], value: {} };
 	exec("arecord -l | grep -w 'card'", function (err, stdout) {
-		if (!err) {
-			fs.readdir(pluginDir, function (err, files) {
-				let l = files.length;
-				for (var index = 0; index < files.length; ++index) {
-					var file = files[index];
-					if (file[0] !== '.') {
-						var filePath = pluginDir + '/' + file + '/config.schema.json';
-						fs.readFile(filePath, 'utf8', function (err, data) {
-							--l;
-							if (!err) {
-								let pluginConfigSchema = JSON.parse(data);
-								if (pluginConfigSchema.schema.speech) {
-									getAudioDevices(pluginConfigSchema, stdout)
-								}
-								Object.assign(configSchema.schema, pluginConfigSchema.schema)
-								if (pluginConfigSchema.form) { configSchema.form = configSchema.form.concat(pluginConfigSchema.form) }
-								if (pluginConfigSchema.value) { Object.assign(configSchema.value, pluginConfigSchema.value) }
+		fs.readdir(pluginDir, function (err, files) {
+			let l = files.length;
+			for (var index = 0; index < files.length; ++index) {
+				var file = files[index];
+				if (file[0] !== '.') {
+					var filePath = pluginDir + '/' + file + '/config.schema.json';
+					fs.readFile(filePath, 'utf8', function (err, data) {
+						--l;
+						if (!err) {
+							let pluginConfigSchema = JSON.parse(data);
+							if (pluginConfigSchema.schema.speech) {
+								getAudioDevices(pluginConfigSchema, stdout)
 							}
-							!l && cb(configSchema)
-						});
-					}
+							Object.assign(configSchema.schema, pluginConfigSchema.schema)
+							if (pluginConfigSchema.form) { configSchema.form = configSchema.form.concat(pluginConfigSchema.form) }
+							if (pluginConfigSchema.value) { Object.assign(configSchema.value, pluginConfigSchema.value) }
+						}
+						!l && cb(configSchema)
+					});
 				}
-			});
-		}
+			}
+		});
 	});
 }
 function getAudioDevices(obj, stdO) {
