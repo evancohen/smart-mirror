61e128c618a3bf7204efb90978790fb5ec7db8e4
Bill Martin <bmartin@salientcdi.com>
2017-04-14 15:58:44 -0500

    Add functionality to custom html thingy

--- plugins/customizer/controller.js
+++ plugins/customizer/controller.js
@@ -1,13 +1,16 @@
-function Customizer($scope, SpeechService) {
+function Customizer($scope, SpeechService, Focus) {
 
-    if (config.customizer && config.customizer.commands.length >= 1) {
-        angular.forEach(config.customizer.commands, function (command) {
-            SpeechService.addRawCommand(command.utterance, function () {
-                $scope.customizerHTML = command.body
-                $scope.$parent.focus = "customizer"
-            });
-        });
-    }
+	if (config.customizer && config.customizer.commands.length >= 1) {
+		angular.forEach(config.customizer.commands, function (command) {
+			SpeechService.addRawCommand(command.utterance, function () {
+				$scope.customizerHTML = command.body
+				$scope.customizerHTML.getText = function(obj) {
+					return obj.split("\n")
+				}
+				Focus.change("customizer")
+			});
+		});
+	}
 }
 
 angular.module('SmartMirror')
