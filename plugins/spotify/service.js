4fbddc60eca1a1cb9f18f72f64c05eba53ecdff4
cyberdude86 <cyberdude86@gmail.com>
2017-12-31 19:58:47 -0500

    testing simple spotify read

--- plugins/spotify/service.js
+++ plugins/spotify/service.js
@@ -11,18 +11,42 @@
 
 		service.init = function () {
       // If the spotify key is defined and not empty
-//			if (typeof config.spotify != 'undefined' && config.spotify.length) {
-                spotify = new SpotifyWebApi({
-                    clientId : config.spotify.id,//'bd604371889f45739b2f684663c6eca1',
-                    clientSecret : config.spotify.secret,//'efb611a216f842919dbf1767be1c2f08',
-                    redirectUri : config.spotify.redirect//'http://localhost:8080',
+			if (typeof config.spotify != 'undefined' && config.spotify.length) {
+                
+                
+                
+                
+                var scopes = ['user-read-private', 'user-read-email'],
+                    redirectUri = 'http://localhost:8080/callback',
+                    clientId = config.spotify.id,
+                    state = 'some-state-of-my-choice';
+
+                // Setting credentials can be done in the wrapper's constructor, or using the API object's setters.
+                var spotify = new SpotifyWebApi({
+                  redirectUri : redirectUri,
+                  clientId : clientId
                 });
-//			}
+
+                // Create the authorization URL
+                var authorizeURL = spotify.createAuthorizeURL(scopes, state);
+
+                // https://accounts.spotify.com:443/authorize?client_id=5fe01282e44241328a84e7c5cc169165&response_type=code&redirect_uri=https://example.com/callback&scope=user-read-private%20user-read-email&state=some-state-of-my-choice
+                console.log(authorizeURL);
+                
+                
+                
+                
+//                spotify = new SpotifyWebApi({
+//                    clientId : config.spotify.id,
+//                    clientSecret : config.spotify.secret,
+//                    redirectUri : config.spotify.redirect
+//                });
+			}
 		}
 
         service.searchSpotify = function (query) {
             // Search tracks whose artist's name contains 'Kendrick Lamar', and track name contains 'Alright'
-            spotify.searchTracks('track:' + query)
+            return spotify.searchTracks('track:' + query)
               .then(function(data) {
                 console.log('Search tracks by "Alright" in the track name and "Kendrick Lamar" in the artist name', data.body);
                 service.spotifyResponse = data;
