a2b1374d40d5ea90f1a18fff1c2d6cd04bedee51
cyberdude86 <cyberdude86@gmail.com>
2018-01-01 14:21:53 -0500

    testing simple spotify read

--- plugins/spotify/service.js
+++ plugins/spotify/service.js
@@ -12,10 +12,6 @@
 		service.init = function () {
       // If the spotify key is defined and not empty
 //			if (typeof config.spotify != 'undefined' && config.spotify.length) {
-                
-
-            
-            
             var clientId = config.spotify.id,
                 clientSecret = config.spotify.secret;
 
@@ -46,57 +42,13 @@
 //
 //                // https://accounts.spotify.com:443/authorize?client_id=5fe01282e44241328a84e7c5cc169165&response_type=code&redirect_uri=https://example.com/callback&scope=user-read-private%20user-read-email&state=some-state-of-my-choice
 //                console.log(authorizeURL);
-                
-            
-//                //PART2
-//                var credentials = {
-//                  clientId : 'someClientId',
-//                  clientSecret : 'someClientSecret',
-//                  redirectUri : 'http://www.michaelthelin.se/test-callback'
-//                };
-//
-//                var spotifyApi = new SpotifyWebApi(credentials);
-//
-//                // The code that's returned as a query parameter to the redirect URI
-//                var code = 'MQCbtKe23z7YzzS44KzZzZgjQa621hgSzHN';
-//
-//                // Retrieve an access token and a refresh token
-//                spotifyApi.authorizationCodeGrant(code)
-//                  .then(function(data) {
-//                    console.log('The token expires in ' + data.body['expires_in']);
-//                    console.log('The access token is ' + data.body['access_token']);
-//                    console.log('The refresh token is ' + data.body['refresh_token']);
-//
-//                    // Set the access token on the API object to use it in later calls
-//                    spotifyApi.setAccessToken(data.body['access_token']);
-//                    spotifyApi.setRefreshToken(data.body['refresh_token']);
-//                  }, function(err) {
-//                    console.log('Something went wrong!', err);
-//                  });
-            
-            
-            
-            
-            
-            
-            
-            
-                
-                
-                
-//                spotify = new SpotifyWebApi({
-//                    clientId : config.spotify.id,
-//                    clientSecret : config.spotify.secret,
-//                    redirectUri : config.spotify.redirect
-//                });
-//			}
 		}
 
         service.searchSpotify = function (query) {
-            // Search tracks whose artist's name contains 'Kendrick Lamar', and track name contains 'Alright'
+            // Search tracks whose name contains the query
             return spotify.searchTracks('track:' + query)
               .then(function(data) {
-                console.log('Search tracks by "Alright" in the track name and "Kendrick Lamar" in the artist name', data.body);
+                console.log('Search tracks matching "' + query + '"', data.body);
                 service.spotifyResponse = data;
                 return service.spotifyResponse;
               }, function(err) {
