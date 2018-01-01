be8d93d4ec4ad99527b00e38e5eb63b788a94a29
cyberdude86 <cyberdude86@gmail.com>
2017-12-31 19:23:27 -0500

    testing simple spotify read

--- plugins/spotify/service.js
+++ plugins/spotify/service.js
@@ -13,9 +13,9 @@
       // If the spotify key is defined and not empty
 //			if (typeof config.spotify != 'undefined' && config.spotify.length) {
                 spotify = new SpotifyWebApi({
-                    clientId : 'bd604371889f45739b2f684663c6eca1',//config.spotify.id,
-                    clientSecret : 'efb611a216f842919dbf1767be1c2f08',//config.spotify.secret,
-                    redirectUri : 'http://localhost:8080',
+                    clientId : config.spotify.id,//'bd604371889f45739b2f684663c6eca1',
+                    clientSecret : config.spotify.secret,//'efb611a216f842919dbf1767be1c2f08',
+                    redirectUri : config.spotify.redirect//'http://localhost:8080',
                 });
 //			}
 		}
