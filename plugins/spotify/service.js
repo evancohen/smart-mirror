52be178892d8cdfabb2fc9ccb06713537e2b1a54
cyberdude86 <cyberdude86@gmail.com>
2018-01-01 13:59:46 -0500

    testing simple spotify read

--- plugins/spotify/service.js
+++ plugins/spotify/service.js
@@ -13,52 +13,65 @@
       // If the spotify key is defined and not empty
 //			if (typeof config.spotify != 'undefined' && config.spotify.length) {
                 
+
+            var spotify = new SpotifyWebApi();
+            spotify.setAccessToken('BQAgbDD_4H801NTJ2aH5d7ZtWsRs4feE7ZYySCtjmycJwzURjsOIkdyTNHFlhkS3BwGsssfEedXRbS-CWA1l_5E1rcN5rQHBU4a5R1g5T1UXlWxK4yn3o-pc1gHpANH_cbzDxEDB6LJr0UEOUGnBolYDF3PldPrLIA');
+            
+            
+            
                 
+//                //PART1
+//                var scopes = ['user-read-private', 'user-read-email'],
+//                    redirectUri = 'http://localhost:8888',
+//                    clientId = config.spotify.id,
+//                    state = 'some-state-of-my-choice';
+//
+//                // Setting credentials can be done in the wrapper's constructor, or using the API object's setters.
+//                var spotify = new SpotifyWebApi({
+//                  redirectUri : redirectUri,
+//                  clientId : clientId
+//                });
+//
+//                // Create the authorization URL
+//                var authorizeURL = spotify.createAuthorizeURL(scopes, state);
+//
+//                // https://accounts.spotify.com:443/authorize?client_id=5fe01282e44241328a84e7c5cc169165&response_type=code&redirect_uri=https://example.com/callback&scope=user-read-private%20user-read-email&state=some-state-of-my-choice
+//                console.log(authorizeURL);
                 
+            
+//                //PART2
 //                var credentials = {
-//                  clientId : config.spotify.id,
-//                  clientSecret : config.spotify.secret,
+//                  clientId : 'someClientId',
+//                  clientSecret : 'someClientSecret',
 //                  redirectUri : 'http://www.michaelthelin.se/test-callback'
 //                };
 //
-//                var spotify = new SpotifyWebApi(credentials);
+//                var spotifyApi = new SpotifyWebApi(credentials);
 //
 //                // The code that's returned as a query parameter to the redirect URI
 //                var code = 'MQCbtKe23z7YzzS44KzZzZgjQa621hgSzHN';
 //
 //                // Retrieve an access token and a refresh token
-//                spotify.authorizationCodeGrant(code)
+//                spotifyApi.authorizationCodeGrant(code)
 //                  .then(function(data) {
 //                    console.log('The token expires in ' + data.body['expires_in']);
 //                    console.log('The access token is ' + data.body['access_token']);
 //                    console.log('The refresh token is ' + data.body['refresh_token']);
 //
 //                    // Set the access token on the API object to use it in later calls
-//                    spotify.setAccessToken(data.body['access_token']);
-//                    spotify.setRefreshToken(data.body['refresh_token']);
+//                    spotifyApi.setAccessToken(data.body['access_token']);
+//                    spotifyApi.setRefreshToken(data.body['refresh_token']);
 //                  }, function(err) {
 //                    console.log('Something went wrong!', err);
 //                  });
-                
-                
-                
-                var scopes = ['user-read-private', 'user-read-email'],
-                    redirectUri = 'http://localhost:8080/callback',
-                    clientId = config.spotify.id,
-                    state = 'some-state-of-my-choice';
-
-                // Setting credentials can be done in the wrapper's constructor, or using the API object's setters.
-                var spotify = new SpotifyWebApi({
-                  redirectUri : redirectUri,
-                  clientId : clientId
-                });
-
-                // Create the authorization URL
-                var authorizeURL = spotify.createAuthorizeURL(scopes, state);
-
-                // https://accounts.spotify.com:443/authorize?client_id=5fe01282e44241328a84e7c5cc169165&response_type=code&redirect_uri=https://example.com/callback&scope=user-read-private%20user-read-email&state=some-state-of-my-choice
-                console.log(authorizeURL);
-                
+            
+            
+            
+            
+            
+            
+            
+            
                 
                 
                 
