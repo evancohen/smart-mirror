20aa8001ac0c069052ade91093f190a9c93563c0
cyberdude86 <cyberdude86@gmail.com>
2018-01-01 22:06:15 -0500

    testing simple spotify read

--- plugins/spotify/oauth/app.js
+++ plugins/spotify/oauth/app.js
@@ -44,7 +44,7 @@ app.get('/login', function(req, res) {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = 'user-read-private user-read-email';
+  var scope = 'user-read-private user-read-email streaming user-read-currently-playing user-modify-playback-state user-read-playback-state';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
