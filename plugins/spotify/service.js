794c2b367c4325d7124158940de75f5333906442
debugskeleton <debugskeleton@gmail.com>
2018-01-07 13:46:33 -0500

    fsa

--- plugins/spotify/service.js
+++ plugins/spotify/service.js
@@ -69,15 +69,15 @@
                 
 
                 // Retrieve an access token and a refresh token
-                spotifyApi.authorizationCodeGrant(code)
+                spotify.authorizationCodeGrant(code)
                   .then(function(data) {
                     console.log('The token expires in ' + data.body['expires_in']);
                     console.log('The access token is ' + data.body['access_token']);
                     console.log('The refresh token is ' + data.body['refresh_token']);
 
                     // Set the access token on the API object to use it in later calls
-                    spotifyApi.setAccessToken(data.body['access_token']);
-                    spotifyApi.setRefreshToken(data.body['refresh_token']);
+                    spotify.setAccessToken(data.body['access_token']);
+                    spotify.setRefreshToken(data.body['refresh_token']);
                     
 //                    // persist the token
 //					persist.write(tokenFile, token, function (err) {
