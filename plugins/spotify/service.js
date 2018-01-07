9b052fea1b09dbbae129231ada8e539c377253d6
debugskeleton <debugskeleton@gmail.com>
2018-01-07 13:45:17 -0500

    fsdf

--- plugins/spotify/service.js
+++ plugins/spotify/service.js
@@ -63,16 +63,43 @@
                 This endpoint is refered to in config.spotify.authorization_uri.redirect_uri.
              */
 			app.get('/spotify_auth_callback', function (req, res, next) {
-				var code = req.query.code;
-				spotify.fetchToken(code, function (err, token) {
-					if (err) return next(err);
+				// The code that's returned as a query parameter to the redirect URI
+                var code = req.query.code;
+                
+                
 
-					// persist the token
-					persist.write(tokenFile, token, function (err) {
-						if (err) return next(err);
-						res.redirect('/fb-profile');
-					});
-				});
+                // Retrieve an access token and a refresh token
+                spotifyApi.authorizationCodeGrant(code)
+                  .then(function(data) {
+                    console.log('The token expires in ' + data.body['expires_in']);
+                    console.log('The access token is ' + data.body['access_token']);
+                    console.log('The refresh token is ' + data.body['refresh_token']);
+
+                    // Set the access token on the API object to use it in later calls
+                    spotifyApi.setAccessToken(data.body['access_token']);
+                    spotifyApi.setRefreshToken(data.body['refresh_token']);
+                    
+//                    // persist the token
+//					persist.write(tokenFile, token, function (err) {
+//						if (err) return next(err);
+//						res.redirect('/fb-profile');
+//					});
+                  }, function(err) {
+                    console.log('Something went wrong!', err);
+                  });
+                
+                
+                
+                
+//				spotify.fetchToken(code, function (err, token) {
+//					if (err) return next(err);
+//
+//					// persist the token
+//					persist.write(tokenFile, token, function (err) {
+//						if (err) return next(err);
+//						res.redirect('/fb-profile');
+//					});
+//				});
 			});
 
             /*
