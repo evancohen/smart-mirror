fe63802150886da8a1f6c1f9ab7d3c15e5311206
debugskeleton <debugskeleton@gmail.com>
2018-01-10 19:44:26 -0500

    fsd

--- plugins/spotify/service.js
+++ plugins/spotify/service.js
@@ -207,7 +207,7 @@
         
         service.playTrack = function (query) {
             console.log(query, typeof query);
-            if (query) {
+            if (typeof query !== 'undefined') {
                 return spotify.play();
             } else {
                 return spotify.searchTracks('track:' + query)
