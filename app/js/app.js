b7c43214f4778980651850d00f615e85f728858d
Sam Detweiler <sdetweil@gmail.com>
2020-06-24 14:30:02 -0500

    add sanitize for plugins who want to show html content

--- app/js/app.js
+++ app/js/app.js
@@ -1,34 +1,55 @@
 // Bootstrap Angular
 (function (angular) {
-	'use strict';
+	"use strict";
 
-	var language = (typeof config.general.language != 'undefined') ? config.general.language.substring(0, 2).toLowerCase() : 'en';
-	angular.module('SmartMirror', ['ngAnimate', 'tmh.dynamicLocale', 'pascalprecht.translate'])
+	var language =
+		typeof config.general.language != "undefined"
+			? config.general.language.substring(0, 2).toLowerCase()
+			: "en";
+	angular
+		.module("SmartMirror", [
+			"ngAnimate",
+			"tmh.dynamicLocale",
+			"pascalprecht.translate",
+			require("angular-sanitize"),
+		])
 		.config(function (tmhDynamicLocaleProvider) {
-			console.log(config)
-			tmhDynamicLocaleProvider.localeLocationPattern('bower_components/angular-i18n/angular-locale_' + language + '.js');
+			console.log(config);
+			tmhDynamicLocaleProvider.localeLocationPattern(
+				"bower_components/angular-i18n/angular-locale_" +
+					language +
+					".js"
+			);
 		})
 
-		.config(['$translateProvider', function ($translateProvider) {
-			$translateProvider
-				.uniformLanguageTag('bcp47')
-				.useStaticFilesLoader({
-					prefix: 'app/locales/',
-					suffix: 'c.json'
-				});
-			$translateProvider.useSanitizeValueStrategy(null);
-			// Avoiding the duplicity of the locale for the default language, xx-YY -> xx
-			// We are considering only the language
-			// Please refer https://github.com/evancohen/smart-mirror/pull/179 for further discussion
-			var language = (typeof config.general.language != 'undefined') ? config.general.language.substring(0, 2) : 'en';
-			$translateProvider.preferredLanguage(language);
-		}])
+		.config([
+			"$translateProvider",
+			function ($translateProvider) {
+				$translateProvider
+					.uniformLanguageTag("bcp47")
+					.useStaticFilesLoader({
+						prefix: "app/locales/",
+						suffix: "c.json",
+					});
+				$translateProvider.useSanitizeValueStrategy(null);
+				// Avoiding the duplicity of the locale for the default language, xx-YY -> xx
+				// We are considering only the language
+				// Please refer https://github.com/evancohen/smart-mirror/pull/179 for further discussion
+				var language =
+					typeof config.general.language != "undefined"
+						? config.general.language.substring(0, 2)
+						: "en";
+				$translateProvider.preferredLanguage(language);
+			},
+		])
 
-		.config(["$sceDelegateProvider", function ($sceDelegateProvider) {
-			$sceDelegateProvider.resourceUrlWhitelist([
-				'self',
-				"https://www.youtube.com/embed/**"
-			]);
-		}]);
-
-} (window.angular));
+		.config([
+			"$sceDelegateProvider",
+			function ($sceDelegateProvider) {
+				$sceDelegateProvider.resourceUrlWhitelist([
+					"self",
+					"https://www.youtube.com/embed/**",
+				]);
+			},
+		]);
+})(window.angular);
