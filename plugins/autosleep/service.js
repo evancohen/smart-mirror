261a5eb22b422731fc5064f4b336cdc597482063
Sam Detweiler <sdetweil@gmail.com>
2021-04-01 11:58:43 -0500

    fix prettier config

--- plugins/autosleep/service.js
+++ plugins/autosleep/service.js
@@ -1,24 +1,24 @@
 /* global ipcRenderer */
-;(function () {
-	"use strict"
+(function () {
+	"use strict";
 
 	function AutoSleepService($interval, Focus) {
-		var service = {}
-		var autoSleepTimer
-		service.woke = true
-		service.scope = "default"
-		service.exec = require("child_process").exec
+		var service = {};
+		var autoSleepTimer;
+		service.woke = true;
+		service.scope = "default";
+		service.exec = require("child_process").exec;
 
-		var energyStarTimer = null
-		var energyStarTimerStop = null
+		var energyStarTimer = null;
+		var energyStarTimerStop = null;
 		// energyStar timeout is 15 minutes
 		// we will wait 14.5 minutes in milliseconds
-		var energyStarDelay = 14.5 * 60 * 1000
+		var energyStarDelay = 14.5 * 60 * 1000;
 		// forced wakeup to defeat TV energystar power off
-		var energyStarWakeupDelay = 2 * 1000
+		var energyStarWakeupDelay = 2 * 1000;
 
 		service.startAutoSleepTimer = function () {
-			var milliConversion = 60000
+			var milliConversion = 60000;
 			// only start timer if enabled, and control values defined
 			if (
 				typeof config.autoTimer !== "undefined" &&
@@ -26,146 +26,146 @@
 				typeof config.autoTimer.autoSleep !== "undefined" &&
 				typeof config.autoTimer.autoWake !== "undefined"
 			) {
-				service.stopAutoSleepTimer()
+				service.stopAutoSleepTimer();
 				// assume if autoSleep is greater than 1 minute in milliseconds the value is already converted. if not convert
 				if (config.autoTimer.autoSleep > 60000) {
-					milliConversion = 1
+					milliConversion = 1;
 					console.info(
 						"ProTip: Change your config so that config.autoTimer.autoSleep is in minutes not milliseconds."
-					)
+					);
 				}
 				autoSleepTimer = $interval(
 					service.sleep,
 					config.autoTimer.autoSleep * milliConversion
-				)
+				);
 				console.debug(
 					"Starting auto-sleep timer",
 					config.autoTimer.autoSleep * milliConversion
-				)
+				);
 			}
-		}
+		};
 
 		service.stopAutoSleepTimer = function () {
-			console.debug("Stopping auto-sleep timer")
-			$interval.cancel(autoSleepTimer)
-		}
+			console.debug("Stopping auto-sleep timer");
+			$interval.cancel(autoSleepTimer);
+		};
 
 		service.wake = function () {
 			if (config.autoTimer.mode !== "disabled") {
 				// only wake up if sleeping
 				if (Focus.get() === "sleep") {
-					service.woke = true
+					service.woke = true;
 					switch (config.autoTimer.mode) {
 						case "monitor":
 						case "tv":
-							service.exec(config.autoTimer.wakeCmd, service.puts)
-							Focus.change("default")
-							break
+							service.exec(config.autoTimer.wakeCmd, service.puts);
+							Focus.change("default");
+							break;
 						case "energy":
-							Focus.change("default")
+							Focus.change("default");
 							// if the timer was running
 							if (energyStarTimer != null) {
 								// stop it
-								$interval.cancel(energyStarTimer)
-								energyStarTimer = null
+								$interval.cancel(energyStarTimer);
+								energyStarTimer = null;
 							}
 							// if the dummy wake up delay is running, stop it too
 							if (energyStarTimerStop != null) {
-								$interval.cancel(energyStarTimerStop)
+								$interval.cancel(energyStarTimerStop);
 							}
-							break
+							break;
 						default:
-							Focus.change("default")
+							Focus.change("default");
 					}
 				}
 			}
-		}
+		};
 
 		// used by the energystar override function
 		// done being awake, go back to sleep again
 		var done = function () {
 			// go back to sleep
-			Focus.change("sleep")
+			Focus.change("sleep");
 			// stop the short term delay timer
-			$interval.cancel(energyStarTimerStop)
+			$interval.cancel(energyStarTimerStop);
 			// cancel to long timer
-			$interval.cancel(energyStarTimer)
+			$interval.cancel(energyStarTimer);
 			// restart it, so we don't drift towards 0 delay
-			energyStarTimer = $interval(bleep, energyStarDelay)
+			energyStarTimer = $interval(bleep, energyStarDelay);
 			// restart the main sleep timer
-			service.startAutoSleepTimer()
-		}
+			service.startAutoSleepTimer();
+		};
 
 		// do the fake, short term wakeup
 		var bleep = function () {
-			Focus.change("default")
+			Focus.change("default");
 			// start the timer for returning to sleep
-			energyStarTimerStop = $interval(done, energyStarWakeupDelay)
-		}
+			energyStarTimerStop = $interval(done, energyStarWakeupDelay);
+		};
 
 		service.sleep = function () {
 			if (config.autoTimer.mode !== "disabled") {
-				service.woke = false
+				service.woke = false;
 				switch (config.autoTimer.mode) {
 					case "monitor":
 					case "tv":
-						break
+						break;
 					case "energy":
-						Focus.change("sleep")
+						Focus.change("sleep");
 						if (energyStarTimer == null) {
-							energyStarTimer = $interval(bleep, energyStarDelay)
+							energyStarTimer = $interval(bleep, energyStarDelay);
 						} else {
-							$interval.cancel(energyStarTimer)
-							energyStarTimer = $interval(bleep, energyStarDelay)
+							$interval.cancel(energyStarTimer);
+							energyStarTimer = $interval(bleep, energyStarDelay);
 						}
-						break
+						break;
 					default:
-						Focus.change("default")
+						Focus.change("default");
 				}
 			} else {
-				Focus.change("default")
+				Focus.change("default");
 			}
-		}
+		};
 
 		service.puts = function (error, stdout, stderr) {
 			if (error) {
-				console.debug("auto-sleep error", error)
+				console.debug("auto-sleep error", error);
 			}
-			console.debug("autosleep stdout:", stdout)
-			console.debug("autosleep stderr:", stderr)
-		}
+			console.debug("autosleep stdout:", stdout);
+			console.debug("autosleep stderr:", stderr);
+		};
 
 		ipcRenderer.on("motionstart", () => {
-			service.wake(true)
-			console.debug("motion start detected")
-		})
+			service.wake(true);
+			console.debug("motion start detected");
+		});
 
 		ipcRenderer.on("remoteWakeUp", () => {
-			service.wake(true)
-			console.debug("remote wakeUp detected")
-		})
+			service.wake(true);
+			console.debug("remote wakeUp detected");
+		});
 
 		ipcRenderer.on("remoteSleep", () => {
-			service.sleep()
-			console.debug("remote sleep detected")
-		})
+			service.sleep();
+			console.debug("remote sleep detected");
+		});
 
 		ipcRenderer.on("motionend", () => {
-			console.debug("motion end detected")
-			service.startAutoSleepTimer()
-		})
+			console.debug("motion end detected");
+			service.startAutoSleepTimer();
+		});
 
 		ipcRenderer.on("calibrated", () => {
-			console.debug("motion.js Calibrated")
-			service.startAutoSleepTimer()
-		})
+			console.debug("motion.js Calibrated");
+			service.startAutoSleepTimer();
+		});
 
 		ipcRenderer.on("Error", (event, error) => {
-			console.debug("Motion", error)
-		})
+			console.debug("Motion", error);
+		});
 
-		return service
+		return service;
 	}
 
-	angular.module("SmartMirror").factory("AutoSleepService", AutoSleepService)
-})()
+	angular.module("SmartMirror").factory("AutoSleepService", AutoSleepService);
+})();
