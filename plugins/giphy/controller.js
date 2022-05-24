d9e6915ab0580a55d07907a0badb32bc0265d88d
Sam Detweiler <sdetweil@gmail.com>
2022-05-24 09:36:44 -0500

    fix giphy and xkcd for api changes

--- plugins/giphy/controller.js
+++ plugins/giphy/controller.js
@@ -4,7 +4,8 @@ function Giphy($scope, $http, SpeechService, Focus) {
 	SpeechService.addCommand('image_giphy', function (img) {
 		$http.get("http://api.giphy.com/v1/gifs/random?api_key=" + config.giphy.key + "&tag=" + img)
 			.then(function (response) {
-				$scope.gifimg = response.data.data.image_url;
+				$scope.gifimg = response.data.data.images.original.url;
+				//console.log(JSON.stringify(response.data.data,null,2))
 				Focus.change("gif");
 			})
 	});
